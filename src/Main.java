46ceb731d2194900a2e2c0cd51fab697bf06f5c8
Pipe Cadavid <cadavid.felipe@hotmail.com>
2019-06-14 16:33:52 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String stringExpression= "This is a print out test";
+
+        System.out.println(stringExpression);
+    }
+}
