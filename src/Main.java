434f169b67ae429369bcb18b30a61bb7c9e23099
Pipe Cadavid <cadavid.felipe@hotmail.com>
2019-06-14 16:42:14 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         String stringExpression= "This is a print out test";
 
+        int numberone = 4;
+        int numbertwo = 6;
+
         System.out.println(stringExpression);
+        System.out.println(numberone*numbertwo);
     }
 }
